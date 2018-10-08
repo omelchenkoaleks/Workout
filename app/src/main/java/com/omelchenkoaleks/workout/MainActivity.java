9602fccce9c70f2c55375c9dfdf6b110d2b3bdfa
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-08 16:15:28 +0300

    Добавлена вторая активити и с первой организован переход на вторую активити при нажатии кнопки.

--- app/src/main/java/com/omelchenkoaleks/workout/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/workout/MainActivity.java
@@ -1,7 +1,9 @@
 package com.omelchenkoaleks.workout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // этот метод вызовется при щелчке по кнопке и откроет указанную активити
+    public void onShowDetails(View view) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 }
