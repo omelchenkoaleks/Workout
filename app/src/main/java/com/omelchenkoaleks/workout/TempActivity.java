639401574af9102735e1a6c3ac3a4b975310f213
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-09 14:16:00 +0300

    создаем транзакцию чтобы при повороте секундомер продолжал работать

--- app/src/main/java/com/omelchenkoaleks/workout/TempActivity.java
+++ app/src/main/java/com/omelchenkoaleks/workout/TempActivity.java
@@ -1,5 +1,6 @@
 package com.omelchenkoaleks.workout;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,17 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+
+        // создаем транзакцию фрагмента для добавления экземпляра StopwatchFragment в TempActivity
+        // проверяем - фрагмент должен быть добавлент только в том случае, если активность не создается
+        // заново после уничтожения:
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatchFragment = new StopwatchFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.stopwatch_container, stopwatchFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        }
     }
 }
