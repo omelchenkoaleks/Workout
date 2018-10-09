a65e9ecbead4814999b13b7912d3c4459f71ce9e
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-09 13:53:50 +0300

    связали слушателя с кнопками

--- app/src/main/java/com/omelchenkoaleks/workout/StopwatchFragment.java
+++ app/src/main/java/com/omelchenkoaleks/workout/StopwatchFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -49,6 +50,15 @@ public class StopwatchFragment extends Fragment implements View.OnClickListener{
                              Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
         runTimer(layout);
+
+        // связывание OnClickListener с кнопками
+        Button startButton = layout.findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+        Button stopButton = layout.findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(this);
+        Button resetButton = layout.findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(this);
+
         return layout;
     }
 
