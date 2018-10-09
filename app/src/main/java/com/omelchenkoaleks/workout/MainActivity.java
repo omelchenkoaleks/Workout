393aed9c6cd67ff4bb365ee76f9a918a339f7521
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-09 12:33:02 +0300

    транзакции фрагментов - пример

--- app/src/main/java/com/omelchenkoaleks/workout/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/workout/MainActivity.java
@@ -1,6 +1,7 @@
 package com.omelchenkoaleks.workout;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,10 +16,15 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     @Override
     public void itemClicked(long id) {
-
         View fragmentContainer = findViewById(R.id.fragment_container);
         if (fragmentContainer != null) {
-            // добавляем фрагмент в FrameLayout
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            details.setWorkoutId(id);
+            fragmentTransaction.replace(R.id.fragment_container, details);
+            // fragmentTransaction(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
         } else {
             Intent intent = new Intent(this, DetailActivity.class);
             intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
